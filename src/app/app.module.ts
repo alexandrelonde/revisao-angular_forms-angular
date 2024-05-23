3f4e7232ebbe061c6849463fc06497de31e75d46
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-05-22 21:14:22 -0300

    feat: criando a estrutura do reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './components/data-form/data-form.component';
+
 import { TemplateFormModule } from './components/template-form/template-form.module';
+import { DataFormModule } from './components/data-form/data-form.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    TemplateFormModule
+    TemplateFormModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './data-form.component.scss'
 })
 export class DataFormComponent {
+
+  // formulario: FormGroup;
+
+  constructor() {
+
+  }
 
 }
