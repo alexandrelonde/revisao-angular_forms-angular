dd887cd5f3320354c20d10079597efcbf3186c1e
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-05-30 10:03:20 -0300

    feat: resetando o formulário e tratando erros

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-data-form',
@@ -10,7 +13,10 @@ export class DataFormComponent implements OnInit {
 
   formulario!: FormGroup;
 
-  constructor(private forbuilder: FormBuilder) { }
+  constructor(
+    private forbuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit() {
     this.formulario = this.forbuilder.group({
@@ -18,4 +24,29 @@ export class DataFormComponent implements OnInit {
       email: [null],
     });
   }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+
+    this.httpClient.post('https://httpbin.org/post', this.formulario.value)
+      .pipe(
+        map(res => res) //map manipula a resposta antes de passar para o subscribe
+      )
+      .subscribe(
+        dados => {
+          console.log(dados);
+          this.formulario.reset(); // reseta os campos do formulário após o envio de dados
+        },
+        error => {
+          console.error(error);
+          alert('Erro ao enviar os dados!');
+        }
+      );
+  }
 }
+
+/*
+  Com a lógica do onSubmit, resetamos o formulário caso os dados sejam
+  enviados com sucesso e caso haja algum erro, é exibida uma mensagem de
+  alerta.
+*/
