91f63fcb4cba7d4cb3b47623625d4e25d2ede254
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-06-03 12:35:38 -0300

    feat: adicionando validação por meio da classe Validators

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -1,43 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrl: './data-form.component.scss'
+  styleUrl: './data-form.component.scss',
 })
 export class DataFormComponent implements OnInit {
-
   formulario!: FormGroup;
 
   constructor(
     private forbuilder: FormBuilder,
     private httpClient: HttpClient
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.formulario = this.forbuilder.group({
-      nome: [null],
-      email: [null],
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
     });
   }
 
   onSubmit() {
     console.log(this.formulario.value);
 
-    this.httpClient.post('https://httpbin.org/post', this.formulario.value)
+    this.httpClient
+      .post('https://httpbin.org/post', this.formulario.value)
       .pipe(
-        map(res => res) //map manipula a resposta antes de passar para o subscribe
+        map((res) => res) //map manipula a resposta antes de passar para o subscribe
       )
       .subscribe(
-        dados => {
+        (dados) => {
           console.log(dados);
           this.formulario.reset(); // reseta os campos do formulário após o envio de dados
         },
-        error => {
+        (error) => {
           console.error(error);
           alert('Erro ao enviar os dados!');
         }
@@ -53,4 +53,18 @@ export class DataFormComponent implements OnInit {
   Com a lógica do onSubmit, resetamos o formulário caso os dados sejam
   enviados com sucesso e caso haja algum erro, é exibida uma mensagem de
   alerta.
+*/
+
+/*
+  ngOnInit() {
+    this.formulario = this.forbuilder.group({
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+    });
+  }
+
+  Para passar mais de uma validação pela classe do Angular chamada Validators,
+  usamos um array, como é mostrado na validação do email. Validators é uma classe
+  importada do @angular/forms', assim como:
+  FormBuilder, FormControl, FormGroup, Validators;
 */
